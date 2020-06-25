3b9d7b0a00ddf4e27ccf95a8ab8a5312b40ac896
arsytamawisa <arsytamawisa@gmail.com>
2020-06-25 17:32:09 +0700

    edit navbar login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,15 +46,15 @@
                               <!-- Authentication Links -->
                               @guest
                               <li class="nav-item">
-                                   <a class="nav-link" href="/quotes">Quotes</a>
+                                   <a class="nav-link text-light" href="/quotes">Quotes</a>
                               </li>
                               <li class="nav-item">
-                                   <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                   <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
                               </li>
 
                               @if (Route::has('register'))
                               <li class="nav-item">
-                                   <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                   <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
                               </li>
                               @endif
 
